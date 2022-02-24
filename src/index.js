64e861ce9cb57685f60f671747306449c6d88dd1
Artem Gorbachev <Ag2003g@yandex.ru>
2022-02-24 22:08:08 +0300

    feat: comlete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2 == 1) return false
+
+  let brackets = []
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    const start = bracketsConfig[i][0]
+    const end = bracketsConfig[i][1]
+
+    if(start == Number(start)) {
+      brackets.push(`${start}${end}`)
+    } else {
+      brackets.push(`\\${start}\\${end}`)
+    }
+    
+  }
+
+  brackets = brackets.join('|')
+  let newStr = str
+
+  for(let i = 0; i < str.length / 2; i++) {
+    newStr = newStr.replace(new RegExp(brackets, 'g'), '')
+  }
+  
+  return !Boolean(newStr)
 }
